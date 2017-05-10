dbfac870781484418e0e8f7cec1eae791875c4e2
Maike Alame <maike.alame@gmail.com>
2017-05-10 13:24:35 -0300

    adapt class Auto for laravel Facades

--- src/AutoWhere.php
+++ src/AutoWhere.php
@@ -9,20 +9,44 @@ use maikealame\AutoWhere\Contracts\AutoWhereInterface;
 class Auto implements AutoWhereInterface
 {
 
-    private $db;
+    // instance for chain methods
+    public $_class;
+    public $_db;
 
     /**
      * Create a new AutoWhere instance.
      */
     public function __construct(){
-        $this->db = Config::get('autowhere.db');
+        $this->_db = (new DB);
     }
 
     /**
      * Initialize module Where
      */
     public function where(){
-        return phpAuto::where();
+        $this->_class = new Where($this);
+        return $this->getInstance();
+    }
+
+
+
+    /**
+     * Generate chain methods
+     *
+     * @return Auto
+     */
+    public function getInstance(){
+        return $this;
+    }
+
+    public function __call($method,$arguments) {
+        if($this->_class) {
+            if (method_exists($this->_class, $method)) {
+                return call_user_func_array(array($this->_class, $method), $arguments);
+            }else return $this;
+        }else{
+            return call_user_func_array(array($this, $method), $arguments);
+        }
     }
 
 }
