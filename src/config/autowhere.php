46f0a24ef84aa8d63bab42db5048e3857067dfcb
Maike Alame <maike.alame@gmail.com>
2017-05-10 13:34:29 -0300

    change config file vars

--- src/config/autowhere.php
+++ src/config/autowhere.php
@@ -2,12 +2,6 @@
 
 return [
 
-    'db' => [
-        'type'               => env("DB_CONNECTION"), // only accept mysql and pgsql string
-        'host'               => env("DB_HOST"),
-        'database'           => env("DB_DATABASE"),
-        'username'           => env("DB_USERNAME"),
-        'pass'               => env("DB_PASSWORD"),
-    ],
+    'db' => env("DB_CONNECTION"), // only accept mysql and pgsql string
 
 ];
