232904d7da62cc6396fca3988d623326bf2fdedb
Maike Alame <maike.alame@gmail.com>
2017-05-11 16:01:21 -0300

    fix error reference to config file

--- src/AutoSortBlade.php
+++ src/AutoSortBlade.php
@@ -25,28 +25,28 @@ class AutoSortBlade
         $sort = $sortOriginal = $param[0];
         $title = $param[1];
 
-        $formatting_function = Config::get('autowhere.sort.formatting_function', null);
+        $formatting_function = Config::get('laravelauto.sort.formatting_function', null);
 
         if (!is_null($formatting_function) && function_exists($formatting_function)) {
             $title = call_user_func($formatting_function, $title);
         }
 
-        $icon = Config::get('autowhere.sort.default_icon_set');
+        $icon = Config::get('laravelauto.sort.default_icon_set');
 
-        foreach (Config::get('autowhere.sort.columns') as $key => $value) {
+        foreach (Config::get('laravelauto.sort.columns') as $key => $value) {
             if (in_array($sort, $value['rows'])) {
                 $icon = $value['class'];
             }
         }
 
         if (Input::get('sort') == $sortOriginal && in_array(Input::get('order'), ['asc', 'desc'])) {
-            $asc_suffix = Config::get('autowhere.sort.asc_suffix', '-asc');
-            $desc_suffix = Config::get('autowhere.sort.desc_suffix', '-desc');
+            $asc_suffix = Config::get('laravelauto.sort.asc_suffix', '-asc');
+            $desc_suffix = Config::get('laravelauto.sort.desc_suffix', '-desc');
             $icon = $icon . (Input::get('order') === 'asc' ? $asc_suffix : $desc_suffix);
             $order = Input::get('order') === 'desc' ? 'asc' : 'desc';
         } else {
-            $icon = Config::get('autowhere.sort.sortable_icon');
-            $order = Config::get('autowhere.sort.default_order_unsorted', 'asc');
+            $icon = Config::get('laravelauto.sort.sortable_icon');
+            $order = Config::get('laravelauto.sort.default_order_unsorted', 'asc');
         }
 
         $param = [
@@ -55,14 +55,14 @@ class AutoSortBlade
         ];
 
         $queryString = http_build_query(array_merge(array_filter(Request::except('sort', 'order', 'page')), $param));
-        $anchorClass = Config::get('autowhere.sort.anchor_class', null);
+        $anchorClass = Config::get('laravelauto.sort.anchor_class', null);
         if ($anchorClass !== null) {
             $anchorClass = 'class="' . $anchorClass . '"';
         }
 
-        $iconAndTextSeparator = Config::get('autowhere.sort.icon_text_separator', '');
+        $iconAndTextSeparator = Config::get('laravelauto.sort.icon_text_separator', '');
 
-        $clickableIcon = Config::get('autowhere.sort.clickable_icon', false);
+        $clickableIcon = Config::get('laravelauto.sort.clickable_icon', false);
         $trailingTag = $iconAndTextSeparator . '<i class="' . $icon . '"></i>' . '</a>' ;
         if ($clickableIcon === false) {
             $trailingTag = '</a>' . $iconAndTextSeparator . '<i class="' . $icon . '"></i>';
