ba132026dd4fd03f9701ea6f32f967b8121bcf50
Maike Alame <maike.alame@gmail.com>
2017-05-11 15:00:25 -0300

    add Exceptions

---
+++ src/Exceptions/AutoSortException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Auto\Exceptions;
+
+use Exception;
+
+class AutoSortException extends Exception
+{
+    public function __construct($message = '', $code = 0, Exception $previous = null)
+    {
+        if ($code === 1) {
+            $message = 'Relation \''. $message . '\' does not exist. Define it in your model.';
+        } elseif ($code === 2) {
+            $message = 'Relation \'' . $message . '\' is not instance of HasOne, HasMany or BelongsTo.';
+        } else {
+            $message = 'Invalid sort argument, explode() did not produce array with size of 2 elements.';
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+}
