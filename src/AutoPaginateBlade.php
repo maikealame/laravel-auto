5877f70d556ffb1e0eee15fe78cf540f0fab21a2
Maike Alame <maike.alame@gmail.com>
2017-05-11 23:20:24 -0300

    fix bind event jquery on pagination a tag

--- src/AutoPaginateBlade.php
+++ src/AutoPaginateBlade.php
@@ -24,7 +24,7 @@ class AutoPaginateBlade
         $selector = isset($param[0]) ? $param[0] : ".panel-table";
         $r = '<script>
         $(document).ready(function(){
-            $(".pagination a").click(function(e){
+            $("body").on("click",".pagination a",function(e){
                 e.preventDefault();
                 var url = $(this).attr("href");
                 $.get(url,{},function(data){
