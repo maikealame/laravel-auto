04b464f9db3ce8f7e89a4323436ca33f53943449
Maike Alame <maike.alame@gmail.com>
2017-08-25 17:02:59 -0300

    fixed bug with setDefaultField

--- src/Auto.php
+++ src/Auto.php
@@ -90,7 +90,8 @@ class Auto implements AutoInterface
      * @return void
      */
     public function setDefaultField($column, $value){
-        Request::has("filter[$column]") ?: Request::merge(["filter"=>array_merge(Request::get("filter")?Request::get("filter"):[],[$column=>$value])]);
+        $filter = Request::get("filter");
+        if($filter) isset($filter[$column]) ?: Request::merge(["filter"=>array_merge( $filter ? $filter : [], [$column=>$value] )]);
     }
 
     /**
